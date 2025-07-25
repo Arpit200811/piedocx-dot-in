b6f46818cddf7c95b2e0aa2edcfb153cfe73d5b9
Arpit200811 <arpitgupta9610@gmail.com>
2025-07-25 23:00:20 +0530

    Added New Files

--- frontend/src/utils/info.js
+++ frontend/src/utils/info.js
@@ -1,2 +1,5 @@
 export const base_url = "https://piedocx-dot-in.onrender.com"
 // export const base_url = "http://localhost:3435"
+export const UI_URL = "https://piedocx-dot-in-1.onrender.com"
+
+// export const UI_URL = "http://localhost:5173"
