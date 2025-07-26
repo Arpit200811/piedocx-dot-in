d5a5cd13133e8de2689925239f502e1083431647
Arpit200811 <arpitgupta9610@gmail.com>
2025-07-26 23:00:53 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -47,8 +47,8 @@ app.get("/", (req, res) => {
 });
 
 // Connect DB and start server
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`✅ Server running at http://localhost:${PORT}`);
+  console.log(`✅ Server running at http://localhost:${5001}`);
   connectDB(); // Connect to MongoDB
 });
